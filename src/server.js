918478fcd21a4bbc65e306c5e1cb84baf50c3255
Michael Z <michaelzwierzanski@gmail.com>
2023-02-22 10:08:00 +0000

    add basic song routes

--- src/server.js
+++ src/server.js
@@ -2,10 +2,26 @@ require("dotenv").config();
 require("./db/connection");
 const express = require("express");
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5001; // two pipes || means or
 
 const app = express();
 
 app.use(express.json());
+
+app.post("/songs", (request, response) => {
+  response.send("Hello from songs post");
+});
+
+app.get("/songs", (request, response) => {
+  response.send("hello from songs get");
+});
+
+app.put("/songs", (request, response) => {
+  response.send("hello from songs put");
+});
+
+app.delete("/songs", (request, response) => {
+  response.send("hello from songs delete");
+});
 
 app.listen(port, () => console.log(`Serveris listening on port ${port}`));
