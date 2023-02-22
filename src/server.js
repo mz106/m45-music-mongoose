c55dfe163aea5542fc34d0bcfcc8cabda48977c4
Michael Z <michaelzwierzanski@gmail.com>
2023-02-22 11:26:43 +0000

    add put route

--- src/server.js
+++ src/server.js
@@ -12,12 +12,15 @@ const app = express();
 app.use(express.json());
 
 app.post("/songs", async (request, response) => {
-  const newSong = await Song.create({
-    songName: request.body.songName,
-    artist: request.body.artist,
-  });
+  // const newSong = await Song.create({
+  //   songName: request.body.songName,
+  //   artist: request.body.artist,
+  // });
+
+  const newSong = await Song.create(request.body);
 
   console.log(newSong);
+
   const successResponse = {
     message: "success",
     newSong: newSong,
@@ -26,12 +29,34 @@ app.post("/songs", async (request, response) => {
   response.send(successResponse);
 });
 
-app.get("/songs", (request, response) => {
-  response.send("hello from songs get");
+app.get("/songs", async (request, response) => {
+  const allSongs = await Song.find({});
+
+  const successResponse = {
+    message: "success",
+    allSongs: allSongs,
+  };
+
+  response.send(successResponse);
 });
 
-app.put("/songs", (request, response) => {
-  response.send("hello from songs put");
+app.put("/songs", async (request, response) => {
+  // const filterObj = { songName: request.body.songName };
+  // const updateObj = { artist: request.body.newArtist };
+
+  const filterObj = { songName: request.body.songName };
+  const updateObj = { [request.body.updateKey]: request.body.updateValue };
+
+  console.log(filterObj, updateObj);
+
+  const updatedSong = await Song.updateOne(filterObj, updateObj);
+
+  const successResponse = {
+    message: "success",
+    updatedSong: updatedSong,
+  };
+
+  response.send(successResponse);
 });
 
 app.delete("/songs", (request, response) => {
